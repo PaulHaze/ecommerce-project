ac20ef5750acf812109bc23595e05fff5acabfaa
PaulHaze <dev@artemist.com.au>
2022-01-13 07:58:34 +1100

    init commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//* ADD THESE FOR REDUX
+// import { Provider } from 'react-redux';
+// import store from './redux/store';
+
+import './styles/main.scss';
+import { Home } from './views';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+    <Home />
+    {/* </Provider> */}
+  </React.StrictMode>,
+
+  document.getElementById('root'),
+);
