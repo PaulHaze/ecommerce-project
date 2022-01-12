7584aa50688ac9d86e531b3855c1d0fb7a976e41
PaulHaze <dev@artemist.com.au>
2022-01-13 10:16:43 +1100

    fix refresh issue, added path alias

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //* ADD THESE FOR REDUX
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import './styles/main.scss';
 import { Home } from './views';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <Home />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <Home />
+    </Provider>
   </React.StrictMode>,
-
   document.getElementById('root'),
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react()],
 });
