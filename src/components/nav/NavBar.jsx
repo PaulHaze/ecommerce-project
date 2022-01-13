9cc714196754e8581553deb549bf3a9c0883846b
PaulHaze <dev@artemist.com.au>
2022-01-14 07:59:44 +1100

    finish styling for directory and nav

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export function NavBar() {
   return (
     <div className="w-full bg-blue-50 p-4">
-      <p>NavBar Placeholder</p>
+      <div className="max-w-xl mx-auto">
+        <p>NavBar Placeholder</p>
+      </div>
     </div>
   );
 }
--- src/components/shopping/ClothingCategory.jsx
+++ src/components/shopping/ClothingCategory.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import styles from './ClothingCategory.module.scss';
+
 export function ClothingCategory({ categoryTitle }) {
   return (
-    <div className="sm:min-w-[30%] min-w-[45%] flex-1 flex-grow m-0.5 p-10 bg-blue-200 flex-center">
-      <div className="px-4 py-3 bg-white/80 text-center">
+    <div className={styles.clothingContainer}>
+      <div className={styles.textContainer}>
         <h2 className="text-2xl text-gray-800/80">
           {categoryTitle.toUpperCase()}
         </h2>
-        <p className="text-gray-800/70 font-light mt-4">SHOP NOW</p>
+        <p className="mt-4 font-light text-gray-800/70">SHOP NOW</p>
       </div>
     </div>
   );
