43d899f97e26cccce69ebeb2a1ccf8fc75f341b7
PaulHaze <dev@artemist.com.au>
2022-01-14 08:11:33 +1100

    extract category to directory component

---
+++ src/data/categories.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    category: 'Hats',
+  },
+  {
+    id: 2,
+    category: 'Jackets',
+  },
+  {
+    id: 3,
+    category: 'Shoes',
+  },
+  {
+    id: 4,
+    category: 'Womens',
+  },
+  {
+    id: 5,
+    category: 'Men',
+  },
+];
--- src/features/directory/Directory.jsx
+++ src/features/directory/Directory.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { ClothingCategory } from '@/components';
 
+import categories from '@/data/categories';
+
 export function Directory() {
+  const renderCategories = categories.map(cat => (
+    <ClothingCategory key={cat.id} categoryTitle={cat.category} />
+  ));
   return (
-    <div>
-      <ClothingCategory />
+    <div className="w-full flex flex-wrap justify-between">
+      {renderCategories}
     </div>
   );
 }
